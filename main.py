d01263b65bf07f52631fdf6250c94609d0fe023e
artyom <art.volyk@gmail.com>
2020-10-24 01:44:30 +0300

    LAST VERSION

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime
+import pandas as pd
+from collections import defaultdict
+
+
+
+goods_raw = pd.read_excel('wine3.xlsx', keep_default_na = False)
+goods_raw.rename(columns = {'Категория':'category','Название':'name', 'Сорт':'sort', 'Цена':'price', 'Картинка':'image', 'Акция':'sale'}, inplace = True)
+goods_raw = goods_raw.to_dict(orient ='records')
+goods_dict = defaultdict(list)
+for i in goods_raw:
+    goods_dict[i['category']].append(i)
+goods_dict = dict(goods_dict)
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -10,10 +21,11 @@ env = Environment(
 
 
 template = env.get_template('template.html')
-founded = int(datetime.date.today().year)-1920
+founded = datetime.today().year - 1920
 
 rendered_page = template.render(
-    date='100'
+    date=founded,
+    goods_dict=goods_dict
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
@@ -21,3 +33,4 @@ with open('index.html', 'w', encoding="utf8") as file:
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
+
